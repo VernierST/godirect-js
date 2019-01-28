ed8cc7ddc98769836e97b3ae471d5c920937c39c
Jordan Austin <jaustin@vernier.com>
2019-01-28 14:46:17 -0800

    Simplify the code further

--- tests/integration/text-encoder-decoder.test.js
+++ tests/integration/text-encoder-decoder.test.js
@@ -3,18 +3,8 @@ const puppeteer = require('puppeteer');
 
 describe('TextEncoder/Decoder', () => {
   test('encodes and decodes correctly', async () => {
-    const browser = await puppeteer.launch({
-      headless: true
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
-    page.emulate({
-      viewport: {
-        width: 500,
-        height: 500
-      },
-      userAgent: ''
-    });
 
     const result = await page.evaluate(() => {
       const uint8array = new TextEncoder().encode('hello world!');
