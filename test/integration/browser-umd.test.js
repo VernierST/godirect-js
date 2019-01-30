63b6f50b5f229309143cd99d056d0e47935ef3b8
Jordan Austin <jaustin@vernier.com>
2019-01-30 11:05:55 -0800

    Fix rollup.config - add tests to verify bundles are working correctly

---
+++ test/integration/browser-umd.test.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+describe('Browser can use UDM bundle', () => {
+  test('godirect object exists', async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.addScriptTag({
+      path: 'dist/godirect.min.umd.js'
+    });
+
+    const result = await page.evaluate(() => {
+      return window.godirect;
+    });
+
+    expect(result).toMatchObject({
+      createDevice: {},
+      selectDevice: {}
+    });
+
+    browser.close();
+  }, 16000);
+});
